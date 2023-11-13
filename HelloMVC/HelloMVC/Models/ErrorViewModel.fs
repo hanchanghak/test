8b84f08fda3004b8331a2df4bc2f5efa44601978
hch <rika0610@naver.com>
2023-11-13 22:52:25 +0900

    [서버]TEST

---
+++ HelloMVC/HelloMVC/Models/ErrorViewModel.fs
@@ -0,0 +1,8 @@
+namespace HelloMVC
+
+open System
+
+type ErrorViewModel private () =
+    member val RequestId : string = null with get, set
+
+    member val ShowRequestId : bool = true with get, set
